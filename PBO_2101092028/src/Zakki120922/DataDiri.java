047b6b0bb372cb3f3c8877fcef2bf074eee41276
Zakki05 <ahmadfaris771@gmail.com>
2022-11-16 15:02:05 +0700

    PBO_2101092028
    Ahmad Faris Almuzakki
    2101092028
    MI 2A

---
+++ PBO_2101092028/src/Zakki120922/DataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Zakki120922;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DataDiri {
+
+    /*Nama :
+      No Bp :
+      Prodi :
+      Jurusan :
+      Alamat :
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama    : Ahmad Faris Almuzakki");
+        System.out.println("No Bp   : 2101092028");
+        System.out.println("Prodi   : Manajemen Informatika");
+        System.out.println("Jurusan : Teknologi Informasi");
+        System.out.println("Alamat  : Ulu Gadut");
+
+        System.out.println("\nNama\t:Ahmad Faris Almuzakki\n"
+                + "No Bp\t:2101092028\n"
+                + "Prodi\t:Manajemen Informatika\n"
+                + "Jurusan\t:Teknologi Informasi\n"
+                + "Alamat\t:Ulu Gadut");
+    }
+}
